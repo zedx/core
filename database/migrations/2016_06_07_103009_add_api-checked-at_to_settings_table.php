526a1b6de5ede03c0c0ae37e152a77a1389549f1
zorx <oudjehih@zedx.org>
2016-06-07 12:44:26 +0200

    add api_checked_at to settings table

---
+++ database/migrations/2016_06_07_103009_add_api-checked-at_to_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddApiCheckedAtToSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->timestamp('api_checked_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->dropColumn('api_checked_at');
+        });
+    }
+}
