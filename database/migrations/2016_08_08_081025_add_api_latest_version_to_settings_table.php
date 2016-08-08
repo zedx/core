b07e357b9112b3415064738ae0cc4345aa6fb1b2
zorx <oudjehih@zedx.org>
2016-08-08 10:18:55 +0200

    add api latest version

---
+++ database/migrations/2016_08_08_081025_add_api_latest_version_to_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddApiLatestVersionToSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->string('api_latest_version')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->dropColumn('api_latest_version');
+        });
+    }
+}
