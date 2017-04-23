3de3409c831358d5df41885ecb4f6e120ef6cbc2
zorx <oudjehih@zedx.org>
2017-04-23 15:36:09 +0200

    add auto approve field

---
+++ database/migrations/2017_03_03_164646_add_auto_approve_to_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddAutoApproveToSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->string('auto_approve')->nullable()->after('language');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->dropColumn('auto_approve');
+        });
+    }
+}
